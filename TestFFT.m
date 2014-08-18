53016b23b79a40e8db8f2009fdac31930fefaf62
eatonemmerich <e.e.metabee@gmail.com>
2014-08-18 15:43:21 +0200

    4 - point FFT correctly processed

--- TestFFT.m
+++ TestFFT.m
@@ -1,14 +1,15 @@
 samplingFreq = 20*10^3; % sampling frequency
 signal_frequency = 240; % radians/sec
 signal_time = 1; % in seconds
-N = signal_time/(1/samplingFreq); % total samples in input signal
+%N = signal_time/(1/samplingFreq); % total samples in input signal
+N = 4;
 t = linspace(0,signal_time,N);
 Signal_no_noise = (cos(signal_frequency*t) + 20*sin((signal_frequency+81)*t));
 f = linspace(-1,0,N/2+1);
 f = [linspace(0,1,N/2) f(1:(N/2))];
-out = fft(Signal_no_noise);
+out = fft(Signal_no_noise)
 figure(1);
-stem (f,real(out));
-out2 = myfft(Signal_no_noise);
+stem (f,abs(out));
+out2 = myfft(Signal_no_noise)
 figure(2);
-
+stem (f,abs(out));
--- myfft.m
+++ myfft.m
@@ -1,20 +1,57 @@
 function Outputsignal = myfft(Inputsignal)
-
+	
+	L = size(Inputsignal,2);
+	Outputsignal = zeros(1,L);
+	% For input of length 4:
+	[Even Odd] = EvenAndOdd(Inputsignal);
+	O1 = twopointfft(Even);		%even
+	O2 = twopointfft(Odd);		%odd
+	% Butterfly is applied to every pair by sequence;
+	SO1 = butterfly(O1(1),O2(1),0,4);
+	SO2 = butterfly(O1(2),O2(2),1,4);
+	%is bit revesed order.
+	Outputsignal = [SO1 SO2];
+	
 	
 endfunction
 
-function Output = butterfly(Input,N,k)
+function Output = butterfly(I1,I2,k,N)
 
-	W_N=e^(-j*2*pi/N)
-	ReductionPhrase = W_N*Input(1);
-	Output(0) = Input(0)+ReductionPhrase;
-	Output(1) = Input(0)-ReductionPhrase; 
+	W_N=e^(-(j*2*pi/N)*k);
+	ReductionPhrase = W_N*I2;
+	Output(1) = I1+ReductionPhrase;
+	Output(2) = I1-ReductionPhrase;
 
 endfunction
 
-function Output = 2pointfft(Input)
+function Output = twopointfft(Input)
 	
-	Output(0) = Input(0)+Input(1);
-	Output(1) = Input(0)-Input(1);
+	Output(1) = Input(1)+Input(2);
+	Output(2) = Input(1)-Input(2);
+
+endfunction
+
+function [Even Odd] = EvenAndOdd(Input)
+
+	length = size(Input,2);
+	x = 1;
+	i = 1;
+	%assuming input data length is always a factor of 2.
+	Even = zeros(1,length/2);
+	Odds = zeros(1,length/2);
+	while x < length
+		Even(i) = Input(x);
+		x = x+1;
+		Odd(i) = Input(x);
+		x = x+1;
+		i = i+1;
+	end
+
+endfunction
+
+function Out = bitreverse(In)
+
+	%test with set function
+	Out = bitrevorder(In);
 
 endfunction
