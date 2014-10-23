53e72912b99b91ffdf7965ffbdb21347a0e76349
eatonemmerich <e.e.metabee@gmail.com>
2014-10-23 17:21:14 +0200

    fixed few mathematical inaccuracies

--- CPUCCMat.m
+++ CPUCCMat.m
@@ -1,17 +1,18 @@
 samplingFreq = 2*10^9; % sampling frequency
-Window_Size = 2^12; % size of window to cross multiply
-number_of_bodies = 5;
+Window_Size = 2^11; % size of window to cross multiply
+%number_of_bodies = 5;
 Num_Of_Antennas = 2;
 signal_frequency = 40*10^8; % radians/sec
-signal_time = 0.00001; % in seconds
+signal_time = 0.00002; % in seconds
 N = signal_time/(1/samplingFreq); % total samples in input signal
 Nsub = mod(N,Window_Size)
 N = N-Nsub
 Nfft = Window_Size%/2 % total samples in FFT output
 Num_Of_Windows_Per_Signal = N/Window_Size;
+P = Num_Of_Windows_Per_Signal
 Number_Of_Signals = Num_Of_Antennas
 Number_Of_Complex_Multiplications = 1/2*(Number_Of_Signals-1)*Number_Of_Signals
-signal_to_noise_input = 0.5; % multiplied to noise then added to signal
+signal_to_noise_input = 0.1; % multiplied to noise then added to signal
 Nyquist_freq = samplingFreq/2;
 t = linspace(0,signal_time,N);
 f = linspace(0,Nyquist_freq,5);
@@ -24,23 +25,23 @@ fs_input = linspace(0,1,Nfft/2);
 %plot(t);
 H = 3; % sizes of pictures.
 W = 4;
-Signal_no_noise = 1*(cos(signal_frequency*t) + cos((signal_frequency+15*10^9)*t)); %frequency noise
+Signal_no_noise = 1*(cos(signal_frequency*t) + cos((signal_frequency-3*10^7)*t)); %frequency noise
 figure(1);
+title('Input signal time domain','fontsize',14);
 subplot(1,2,1);
 plot(t(1:Nfft),abs(Signal_no_noise(1:Nfft)));
-title('Input signal time domain','fontsize',14);
 xlabel('time (s)','fontsize',14);
 set(1,'paperunits','inches');
 set(1,'paperorientation','portrait')
 set(1,'papersize',[H,W]);
 subplot(1,2,2);
 plot(t(1:Nfft),angle(Signal_no_noise(1:Nfft)));
-print(1,'input time.svg','-dsvg');
+saveas(1,'input time.eps','eps');
 figure(2);
+title('Input signal frequency domain','fontsize',14);
 subplot(1,2,1);
 fft_in = Signal_no_noise(1:Nfft);
-plot(fs_input,20*log(abs(fft(fft_in)(1:Nfft/2))));
-title('Input signal frequency domain','fontsize',14);
+plot(fs_input,20*log(abs(fft(fft_in)(1:Nfft/2)))/Nfft);
 xlabel('f_s','fontsize',14);
 set(2,'paperunits','inches');
 set(2,'paperorientation','portrait');
@@ -49,17 +50,47 @@ subplot(1,2,2);
 stem(fs_input,angle(fft(fft_in))(1:Nfft/2));
 xlabel('f_s');
 ylabel('angle');
-print(2,'input freq.svg','-dsvg');
+saveas(2,'input freq.eps','eps');
 %windowcoef = (Nfft)/f; % commonly used constant
-Signal_no_noise_s = csvread('inputtest.csv');
+Signal_no_noise_s = csvread('Inputtwodim4test');
 Signal_no_noise_s = Signal_no_noise_s(:,1:N);
+figure(102);
+title('Antenna input signal 1 freq');
+subplot(1,2,1);
+fft_in = Signal_no_noise_s(1,1:Nfft);
+plot(fs_input,20*log(abs(fft(fft_in)(1:Nfft/2)/Nfft)));
+xlabel('f_s','fontsize',14);
+set(102,'paperunits','inches');
+set(102,'paperorientation','portrait');
+set(102,'papersize',[H,W]);
+subplot(1,2,2);
+stem(fs_input,angle(fft(fft_in))(1:Nfft/2));
+xlabel('f_s');
+ylabel('angle');
+saveas(102,'freqofantennain1.eps','eps');
+figure(103);
+title('Antenna input signal 3 freq');
+subplot(1,2,1);
+fft_in = Signal_no_noise_s(1,1:Nfft);
+plot(fs_input,20*log(abs(fft(fft_in)(1:Nfft/2))/Nfft));
+xlabel('f_s','fontsize',14);
+set(102,'paperunits','inches');
+set(102,'paperorientation','portrait');
+set(102,'papersize',[H,W]);
+subplot(1,2,2);
+stem(fs_input,angle(fft(fft_in))(1:Nfft/2));
+xlabel('f_s');
+ylabel('angle');
+saveas(103,'freqofantennain2.eps','eps');
 Signalf = zeros(Num_Of_Windows_Per_Signal,Window_Size);
 Signalfft = zeros(Number_Of_Signals,Nfft/2);
 Signalmul = zeros(Number_Of_Complex_Multiplications,Nfft/2);
 Signalout = zeros(1,Nfft/2);
 Signalin = zeros(Number_Of_Signals,N);
 analysis_filter = sinc(((0:(N-1))-N/2)/Window_Size);
-HanningWindow = 1/2-(1/2)*cos(2*pi*(0:(N-1))/N);
+%HanningWindow =0.53836-(0.46164)*cos(2*pi*(0:(N-1))/N);%EDIT, replaced with Hamming window
+%EDIT replaced Hanning/hamming with blackman window.
+HanningWindow = 0.42659 - 0.49656*cos(2*pi*(0:N-1)/N)+0.076849*cos(4*pi*(0:N-1)/N);
 pre_filter = analysis_filter.*HanningWindow;
 figure(48);
 plot(t(1:N),pre_filter);
@@ -68,7 +99,7 @@ xlabel('time(s)');
 set(48,'paperunits','inches');
 set(48,'paperorientation','portrait');
 set(48,'papersize',[H,W]);
-print(48,'polyphasepre.svg','-dsvg');
+saveas(48,'polyphasepre.eps','eps');
 % iterate every other signal
 
 % Set up realtime signals
@@ -78,23 +109,25 @@ for x = 1:Num_Of_Antennas
     %for(z = 1:number_of_bodies)
     %    Signal_no_noise_s = Signal_no_noise_s + noise2(z)*e.^((((2*pi*f(z)*t)+noise1(z)*0.5*pi*(x-1))*i));
     %end
-    Signalt = ((Signal_no_noise)+20*(Signal_no_noise_s(x))/signal_to_noise_input);
+    Signalt = ((Signal_no_noise)+(Signal_no_noise_s(x,:))/signal_to_noise_input);
 %add gaussian noise
-    Signalt = Signalt + 0.5*rand(1,N);
+    gaussnoise = rand(1,N);
+    gaussnoise = gaussnoise - mean(gaussnoise);
+    Signalt = Signalt + gaussnoise;
     figure(x+2);
     subplot(1,2,1);
-    plot(t(1:N),abs(Signalt(1:N)));
+    plot(t(1:N),abs(fft(Signalt(1:N))/N));
     xlabel('time(s)','fontsize',14);
     title('Antenna output signal','fontsize',14);
     set((x+2),'paperunits','inches');
     set((x+2),'paperorientation','portrait');
     set((x+2),'papersize',[H,W]);
     subplot(1,2,2);
-    plot(t(1:N),angle(Signalt(1:N)));
+    plot(t(1:N),angle(fft(Signalt(1:N))));
     xlabel('time(s)');
     ylabel('angle');
-    name = ['a' num2str(x) 'outf.svg'];
-    print(x+2,name,'-dsvg');
+    name = ['a' num2str(x) 'outf.eps'];
+    saveas(x+2,name,'eps');
     Signalin(x,:) = Signalt;
 
     %create overlap sequences:
@@ -108,17 +141,17 @@ for x = 1:Num_Of_Antennas
     while window_c <= N
         Signalf(z,:) = (Signalt(start:(window_c)));
 %       Signalf(z,:) = Signalt(1:Nfft);
-		pre_part = pre_filter(start:(window_c));
-		% add prefilter
-		Signalf(z,:) = Signalf(z,:).*pre_part;
-		%figure(z+100*x);
-		%plot(angle(fft(Signalt(start:window_c))));
+	pre_part = pre_filter(start:(window_c));
+	% add prefilter
+	Signalf(z,:) = Signalf(z,:).*pre_part;
+	%figure(z+100*x);
+	%plot(angle(fft(Signalt(start:window_c))));
         start = window_c+1;
         window_c = window_c + Window_Size;
         TEMP = TEMP + Signalf(z,:);
         z = z + 1;
     end
-    TEMP = fft(hilbert(TEMP/(z-1)))/Nfft;
+    TEMP = fft(TEMP/P)/Nfft;
     Signalfft(x2,:) = TEMP(1:Nfft/2);
     x2 = x2 + 1;
     
@@ -129,18 +162,19 @@ y = 0;
 for x = 1:Num_Of_Antennas
 	figure(x+2+Num_Of_Antennas);
 	subplot(1,2,1);
-	plot(fs,abs(Signalfft(x,1:Nfft/2)));
+	plot(20*log(abs(Signalfft(x,1:Nfft/2))));
 	title('after polyphase filter','fontsize',14);
 	xlabel('f_s','fontsize',14);
+	ylabel('dB','fontsize',14);
 	set(x+2+Num_Of_Antennas,'paperunits','inches');
 	set(x+2+Num_Of_Antennas,'paperorientation','portrait')
 	set(x+2+Num_Of_Antennas,'papersize',[H,W]);
-	name = ['pffb-' num2str(x) '.svg'];
+	name = ['pffb-' num2str(x) '.eps'];
 	subplot(1,2,2);
 	stem(fs,angle(Signalfft(x,1:Nfft/2)));
         xlabel('f_s');
 	ylabel('angle');
-	print((x+2+Num_Of_Antennas),name,'-dsvg');
+	saveas((x+2+Num_Of_Antennas),name,'eps');
 	%figure(96+x);
 	%plot(fs,angle(Signalfft(1,1:Nfft/2).*conj(Signalfft(2,1:Nfft/2))));
 end
@@ -181,7 +215,7 @@ subplot(1,2,2);
 stem(fs,angle(Signalout(1:Nfft/2)));
 xlabel('f_s');
 ylabel('angle');
-print(21,'outf.svg','-dsvg');
+saveas(21,'outf.eps','eps');
 
 figure (22);
 plot(angle(Signalout(1:Nfft/2)));
@@ -191,7 +225,7 @@ hold on;
 xlabel('f_s');
 ylabel('angle');
 hold off;
-print(22,'output angle.svg','-dsvg');
+saveas(22,'output angle.eps','eps');
 figure (23);
 size14 = abs(Signalout(10))
 angle14 = angle(Signalout(10))
@@ -208,4 +242,4 @@ hold on;
 xlabel('f_s');
 ylabel('angle');
 hold off;
-print(23,'actual angles.svg','-dsvg');
+saveas(23,'actual angles.eps','eps');
--- createSignal.m
+++ createSignal.m
@@ -1,40 +1,50 @@
-tf = 1/(2*10^9);
 %ts = 0.000001;
 %N = ts/(tf); % total samples in input signal
-N = 4096*16
-ts = N*tf;
+Plength = 512; %length of the amount of data being read of 4096 samples.
+N = 4096*Plength
 answr = zeros(2,N);
-tN = linspace(0,ts,N);
-c = 299792458;
-freq_range = 0.8*10^9;
 freq_res = 1000;
+freq_range = (N/2)*freq_res
+tf = 2*freq_range;
+ts = N*(1/tf);
+tN = linspace(0,ts,N);
+
 %tsin = linspace(0,1,freq_range/freq_res);
 %tetha = sin(pi*tsin);
-f = 0.1*10^9;
-countsamples = (freq_range-f)/freq_res
-damp = gaussian(countsamples,1/countsamples*10);
+f = 1;
+damp = gaussian(freq_range,1/freq_range);
 x = 1;
 TEMP = zeros(2,N);
 while f < freq_range
 %or x = (pcc-1)*freq_range_div_sig+1:pcc*freq_range_div_sig
-	Temp2 = damp(x);
-	TEMP(2,:) = Temp2*cos(2*pi*f*(tN)+0.5);
-	TEMP(1,:) = Temp2*cos(2*pi*f*(tN));
-	answr += TEMP;
-	x++;
-	f = f + freq_res;
-end
-%[A, B] = butter(5,0.9);
 
-%answr(1,:) = filter(A,B,answr(1,:));
-%answr(2,:) = filter(A,B,answr(2,:));
+	if x > 1
+		Temp2 = 1;%damp(x);
+		TEMP(2,:) = Temp2*cos(2*pi*f*(tN));
+		TEMP(1,:) = Temp2*sin(2*pi*f*(tN));
+		answr += TEMP;
+		x++;
+		f = f + freq_res;
+	else
+		Temp2 = 0;
+%		TEMP(2,:) = Temp2*cos(2*pi*f*(tN)+pi);
+%		TEMP(1,:) = Temp2*cos(2*pi*f*(tN));
+		answr += 0;
+		x++;
+		f = freq_res;
+	endif
+end
+[A, B] = butter( 5 , [0.3 0.6] );
+
+answr(1,:) = filter(A,B,answr(1,:));
+answr(2,:) = filter(A,B,answr(2,:));
 answr(:,:)/((freq_range)/freq_res)/N;
 csvwrite('Inputtwodim4',answr);
 figure(1);
 plot(20*log(abs(fft(answr(1,:)/N))));
 figure(2);
-plot(angle(fft(answr(1,:))));
+plot(angle(fft(answr(1,:)))/pi);
 figure(3);
 plot(20*log(abs(fft(answr(2,:)/N))));
 figure(4);
-plot(angle(fft(answr(2,:))));
+plot(angle(fft(answr(2,:)))/pi);
